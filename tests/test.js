e5f04d15b3b22bf2155f4ddc0fa9288afc7f5e5d
Kelvis123456 <93103633+Kelvis123456@users.noreply.github.com>
2026-04-17 17:10:39 -0400

    Add automated task management tests

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,32 @@
-console.log("Prueba automatizada ejecutada correctamente");
+function testCrearTarea() {
+    const tarea = "Estudiar";
+    if (tarea !== "") {
+        console.log("Crear tarea: OK");
+    } else {
+        console.log("Crear tarea: FAIL");
+    }
+}
+
+function testCompletarTarea() {
+    let completada = true;
+    if (completada) {
+        console.log("Completar tarea: OK");
+    } else {
+        console.log("Completar tarea: FAIL");
+    }
+}
+
+function testEliminarTarea() {
+    let tareas = ["A", "B"];
+    tareas.pop();
+
+    if (tareas.length === 1) {
+        console.log("Eliminar tarea: OK");
+    } else {
+        console.log("Eliminar tarea: FAIL");
+    }
+}
+
+testCrearTarea();
+testCompletarTarea();
+testEliminarTarea();
